673a2d893cd12755bd37453ae226ef8c620315e8
Rene M. Paccha <1162669+renemarcelo@users.noreply.github.com>
2018-12-13 00:21:02 -0600

    updating ci file

---
+++ src/services/config.js.example.js
@@ -0,0 +1,6 @@
+export const airtableConfig = {
+  endpointUrl: 'https://api.airtable.com',
+  readonlyKey: process.env.POI_APP_READONLY_KEY,
+  workspaceBios: process.env.POI_APP_WORKSPACE_BIOS,
+  workspaceGigs: process.env.POI_APP_WORKSPACE_GIGS
+}
