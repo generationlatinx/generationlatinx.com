68f5097c5443f415af3720c761fb32a43d896566
Rene M. Paccha <1162669+renemarcelo@users.noreply.github.com>
2018-12-14 20:07:25 -0600

    sticking to using readonly keys

---
+++ src/services/config.js
@@ -0,0 +1,6 @@
+export const airtableConfig = {
+  endpointUrl: 'https://api.airtable.com',
+  readonlyKey: 'keyI94TZHjgSypBej',
+  workspaceBios: 'appL2lbxsCHqyIHbr',
+  workspaceGigs: 'appkPCKODFqbbv4Gk'
+}
