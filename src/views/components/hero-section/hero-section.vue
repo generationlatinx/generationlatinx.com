75737bea5f33230b841ee4bc1fb00a576802a663
Rene M. Paccha <1162669+renemarcelo@users.noreply.github.com>
2018-12-15 19:35:57 -0600

    tablets get a hero

--- src/views/components/hero-section/hero-section.vue
+++ src/views/components/hero-section/hero-section.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="container-init-hero">
-    <img src="@/images/GLx-Logo-Expanded-White.png"
-    srcset="@/images/GLx-Logo-Expanded-White-p-500.png 500w, @/images/GLx-Logo-Expanded-White.png 1200w"
-    sizes="(max-width: 767px) 100vw, 451.20001220703125px"
-    alt="Generation Latinx Logo Mobile"
-    class="responsive-img image-white-banner hide-on-med-only hide-on-med-and-down hide-on-small-only">
+
+  <div class="hero-banner">
+
+    <div>
+      <div class="container-init-hero hide-on-med-and-down container">
+        <img src="@/images/GLx-Logo-Expanded-White.png"
+        srcset="@/images/GLx-Logo-Expanded-White-p-500.png 500w, @/images/GLx-Logo-Expanded-White.png 1200w"
+        sizes="(max-width: 767px) 100vw, 451.20001220703125px"
+        alt="Generation Latinx Logo Mobile"
+        class="responsive-img image-white-banner hide-on-med-only hide-on-med-and-down hide-on-small-only">
+      </div>
+    </div>
+
+
+    <div class="container-wrap-hero container">
+      <div>
+        <!-- header attaches here -->
+        <h1 class="hero-heading"></h1>
+      </div>
+    </div>
 
   </div>
+
+
 
 
 </template>
