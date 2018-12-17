02d62f21ea9a33cbfee959cc1d613dc403418716
Rene M. Paccha <1162669+renemarcelo@users.noreply.github.com>
2018-12-16 22:54:44 -0600

    created localVue router in tests

--- src/views/components/cast-roster/cast-roster.test.js
+++ src/views/components/cast-roster/cast-roster.test.js
@@ -1,15 +1,28 @@
 // You need to install vue-test-utils
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 import CastRoster from './cast-roster.vue'
+
+import VueRouter from 'vue-router'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
+
 
 describe('CastRoster', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(CastRoster)
+    const wrapper = mount(CastRoster, {
+      localVue,
+      router
+    })
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
   test('has correct initial content', () => {
-    const wrapper = mount(CastRoster)
+    const wrapper = mount(CastRoster, {
+      localVue,
+      router
+    })
 
     expect(wrapper.text()).toBe('')
     expect(wrapper.html()).toBe('<div class="container"><div class="row"><div class="s12"><span name="list" tag="div" appear=""></span></div></div></div>')
