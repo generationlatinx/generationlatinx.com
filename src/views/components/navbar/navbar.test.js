984435e98ba6ea46ebe0b5cb8fa59ed33c2f8cee
Rene M. Paccha <1162669+renemarcelo@users.noreply.github.com>
2018-12-16 22:54:44 -0600

    created localVue router in tests

--- src/views/components/navbar/navbar.test.js
+++ src/views/components/navbar/navbar.test.js
@@ -1,15 +1,28 @@
 // You need to install vue-test-utils
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 import NavBar from './navbar.vue'
+import VueRouter from 'vue-router'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+
+const router = new VueRouter()
+
 
 describe('NavBar', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(NavBar)
+    const wrapper = mount(NavBar, {
+      localVue,
+      router
+    })
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
   test('has correct content', () => {
-    const wrapper = mount(NavBar)
+    const wrapper = mount(NavBar, {
+      localVue,
+      router
+    })
     expect(wrapper.text()).toBe('Home Videos Meet the Directors Contact Next show on Tues, 12/10 8pm')
   })
 })
