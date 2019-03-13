b1be7df6c6c0dbd17d7b5f193453211a142b8829
Rene M. Paccha <1162669+renemarcelo@users.noreply.github.com>
2019-03-13 00:48:38 -0500

    rendering animate

--- src/views/components/cast-roster/cast-roster.vue
+++ src/views/components/cast-roster/cast-roster.vue
@@ -2,19 +2,27 @@
   <div class="container">
     <div class="row">
       <div class="s12">
-        <transition-group name="list" tag="div" appear>
-          <div v-for="(bio, index) in bios" :key="index + '_' + bio.id">
-            <router-link
-            :to="{
+        <transition-group
+        name="staggered-fade"
+        tag="div"
+        v-bind:css="false"
+        v-on:before-enter="beforeEnter"
+        v-on:enter="enter"
+        v-on:leave="leave"
+        appear
+        >
+        <div v-for="(bio, index) in bios" :key="index + '_' + bio.id" v-bind:data-index="index">
+          <router-link
+          :to="{
 
-              name: 'Performer',
-              params: {
-                id: bio.id,
-                performer: bio.fields.Performer,
-                performerSlug: bio.fields.Performer.replace(/\W+/g, '-').toLowerCase()
-                } }">
+            name: 'Performer',
+            params: {
+              id: bio.id,
+              performer: bio.fields.Performer,
+              performerSlug: bio.fields.Performer.replace(/\W+/g, '-').toLowerCase()
+              } }">
               <div class="grid-fours">
-                  <performer :bio="bio"></performer>
+                <performer :bio="bio"></performer>
               </div>
             </router-link>
           </div>
@@ -29,7 +37,32 @@ import Performer from '@/views/components/performer'
 
 export default {
   name: 'cast-roster',
+  methods: {
+    beforeEnter: function (el) {
+      el.style.opacity = 0
 
+    },
+    enter: function (el, done) {
+      var delay = el.dataset.index * 250
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 1},
+          { complete: done }
+        )
+      }, delay)
+    },
+    leave: function (el, done) {
+      var delay = el.dataset.index * 250
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 0 },
+          { complete: done }
+        )
+      }, delay)
+    }
+  },
   components: {
     Performer
   },
